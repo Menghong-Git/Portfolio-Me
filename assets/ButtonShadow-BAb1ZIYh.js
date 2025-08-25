2c276ecc37d99cc1fa3b03d3f06e8bafb60340cc
Pen Menghong <penmenghong7@gmail.com>
2025-08-25 14:47:32 +0700

    Updates

---
+++ assets/ButtonShadow-BAb1ZIYh.js
@@ -0,0 +1,36 @@
+import{j as t,d as r}from"./index-DjjJ9hQa.js";import{c as o}from"./createLucideIcon-DUl6JweF.js";/**
+ * @license lucide-react v0.536.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=[["path",{d:"M12 2v13",key:"1km8f5"}],["path",{d:"m16 6-4-4-4 4",key:"13yo43"}],["path",{d:"M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8",key:"1b2hhj"}]],n=o("share",a),d=({title:e})=>t.jsx(i,{children:t.jsxs("button",{className:"flex gap-5 justify-center items-center",children:[e,t.jsx(n,{})]})}),i=r.div`
+  button {
+    width: 165px;
+    height: 62px;
+    cursor: pointer;
+    color: #fff;
+    font-size: 17px;
+    border-radius: 2rem;
+    border: none;
+    position: relative;
+    background: #100720;
+    transition: 0.1s;
+  }
+
+  button::after {
+    content: '';
+    width: 100%;
+    height: 100%;
+    background-image: radial-gradient( circle farthest-corner at 10% 20%, oklch(63.7% 0.237 25.331) 50.8%, oklch(44.4% 0.177 26.899) 100.2% );
+    filter: blur(15px);
+    z-index: -1;
+    position: absolute;
+    left: 0;
+    top: 0;
+  }
+
+  button:active {
+    transform: scale(0.9) rotate(3deg);
+    background: radial-gradient( circle farthest-corner at 10% 20%,  oklch(63.7% 0.237 25.331) 17.8%, oklch(44.4% 0.177 26.899)100.2% );
+    transition: 0.5s;
+  }`;export{d as B};
---
+++ assets/index-cPrHuhDu.js
@@ -0,0 +1,132 @@
+import{r as s,j as e,d as z,m as n}from"./index-DjjJ9hQa.js";import{B as j}from"./ButtonShadow-BAb1ZIYh.js";import{S as F}from"./send-CuvFDQs1.js";import"./createLucideIcon-DUl6JweF.js";const v=s.memo(({children:t,className:r="",colors:l=["#FF5A57","#E02F75","#FF6B90","#FF0000"],speed:o=1})=>{const c={backgroundImage:`linear-gradient(135deg, ${l.join(", ")}, ${l[0]})`,WebkitBackgroundClip:"text",WebkitTextFillColor:"transparent",animationDuration:`${10/o}s`};return e.jsxs("span",{className:`relative inline-block ${r}`,children:[e.jsx("span",{className:"sr-only",children:t}),e.jsx("span",{className:"relative animate-aurora bg-[length:200%_auto] bg-clip-text text-transparent",style:c,"aria-hidden":"true",children:t})]})});v.displayName="AuroraText";const I="/Portfolio-Me/assets/image%20copy%2025-dc9E4J2l.png",M=()=>e.jsx(V,{children:e.jsxs("div",{className:"card",children:[e.jsx("img",{className:" w-full h-full rounded-2xl",src:I}),e.jsxs("div",{className:"content",children:[e.jsxs("p",{className:"title",children:["Pen Menghong",e.jsx("br",{}),e.jsx("span",{children:"Web Developer"})]}),e.jsxs("ul",{className:"sci",children:[e.jsx("li",{children:e.jsx("a",{href:"https://www.facebook.com/share/1EEeL9TCFs/",children:e.jsx("svg",{className:"fa-brands fa-facebook",width:20,xmlns:"http://www.w3.org/2000/svg",viewBox:"0 0 320 512",children:e.jsx("path",{d:"M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"})})})}),e.jsx("li",{children:e.jsxs("a",{href:"https://t.me/+85586623507",children:[e.jsx("img",{className:"fa-brands fa-twitter",width:24}),e.jsx(F,{})]})}),e.jsx("li",{children:e.jsx("a",{href:"https://www.linkedin.com/in/pen-menghong-301b71341?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",children:e.jsx("svg",{className:"fa-brands fa-linkedin-in",width:24,xmlns:"http://www.w3.org/2000/svg",viewBox:"0 0 448 512",children:e.jsx("path",{d:"M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"})})})}),e.jsx("li",{children:e.jsx("a",{href:"https://www.instagram.com/poki_copy?igsh=MWRwMGVuZzNlNzNzMA==",children:e.jsx("svg",{className:"fa-brands fa-instagram",width:24,xmlns:"http://www.w3.org/2000/svg",viewBox:"0 0 448 512",children:e.jsx("path",{d:"M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"})})})})]})]})]})}),V=z.div`
+  .card {
+    position: relative;
+    width: 300px;
+    height: 450px;
+    background: #f00;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 10%; /* Rounded corners for card */
+    overflow: hidden; /* Clip image and gradients to rounded corners */
+  }
+
+  .card::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(#D29494, #FF6B90, #ff0058);
+    filter: blur(30px);
+  }
+
+  .card::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(#D29494, #FF6B90, #ff0058);
+    filter: blur(30px);
+  }
+
+  .card b {
+    position: absolute;
+    inset: 6px;
+    background: rgba(0, 0, 0, 0.6);
+    z-index: 2;
+  }
+
+  .card img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    z-index: 2;
+    transition: 0.5s;
+  }
+
+  .card:hover img {
+    scale: 0.5;
+    opacity: 0.9;
+    transform: translateY(-70px);
+  }
+
+  .card .content {
+    position: absolute;
+    z-index: 3;
+    bottom: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    transform: scale(0);
+    transition: 0.5s;
+  }
+
+  .card:hover .content {
+    transform: scale(1);
+    bottom: 25px;
+  }
+
+  .content .title {
+    position: relative;
+    color: #fff;
+    font-weight: 500;
+    line-height: 1em;
+    font-size: 1em;
+    letter-spacing: 0.1em;
+    text-transform: uppercase;
+    text-align: center;
+  }
+
+  .content .title span {
+    font-weight: 300;
+    font-size: 0.7em;
+  }
+
+  .content .sci {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 8px;
+    margin-top: 5px;
+  }
+
+  .sci li {
+    list-style: none;
+  }
+
+  .sci li .fa-brands {
+    width: 14px;
+  }
+
+  .sci li .fa-facebook {
+    width: 10px;
+  }
+
+  .sci li a {
+    position: relative;
+    text-decoration: none;
+    color: rgba(255, 255, 255, 0.5);
+    background: rgba(0, 0, 0, 0.2);
+    fill: currentColor;
+    width: 30px;
+    height: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 4px;
+    transition: 0.5s;
+  }
+
+  .sci li a:hover {
+    fill: currentColor;
+    color: rgba(255, 255, 255, 1);
+  }
+`;function T({text:t,speed:r=100,cursor:l="|",loop:o=!1,deleteSpeed:c=50,delay:p=1500,className:b}){const[x,f]=s.useState(""),[m,u]=s.useState(0),[h,g]=s.useState(!1),[y,N]=s.useState(0),w=Array.isArray(t)?t:[t],d=w[y]||"";return s.useEffect(()=>{if(!d)return;const k=setTimeout(()=>{h?x.length>0?f(i=>i.slice(0,-1)):(g(!1),u(0),N(i=>(i+1)%w.length)):m<d.length?(f(i=>i+d[m]),u(i=>i+1)):o&&setTimeout(()=>g(!0),p)},h?c:r);return()=>clearTimeout(k)},[m,h,d,o,r,c,p,x,t]),e.jsxs("span",{className:b,children:[x,e.jsx("span",{className:"animate-pulse",children:l})]})}const a={hidden:{opacity:0,y:50},visible:(t=0)=>({opacity:1,y:0,transition:{duration:2,ease:"easeOut",delay:t}})},C=()=>e.jsxs("div",{className:"flex flex-col md:flex-row justify-center items-center md:space-x-16 p-6",children:[e.jsxs("div",{className:"flex flex-col items-start space-y-6 md:w-1/2",children:[e.jsx(n.div,{variants:a,custom:.1,initial:"hidden",whileInView:"visible",viewport:{once:!0},children:e.jsx(v,{className:"text-3xl font-bold",children:"Welcome,"})}),e.jsx(n.div,{variants:a,custom:.3,initial:"hidden",whileInView:"visible",viewport:{once:!0},children:e.jsx(T,{text:["Hello !","I'm MengHong"],speed:100,loop:!0,className:"text-5xl md:text-8xl font-bold flex gap-2"})}),e.jsxs(n.div,{variants:a,custom:.5,initial:"hidden",whileInView:"visible",viewport:{once:!0},className:"space-y-0 mb-12 text-gray-700 dark:text-gray-200",children:[e.jsx("p",{className:"text-lg max-w-md",children:"I'm a Full Stack developer specializing in"}),e.jsx("p",{className:"text-lg mb-4 max-w-md",children:"React and TypeScript."})]}),e.jsxs(n.div,{variants:a,custom:.9,initial:"hidden",whileInView:"visible",viewport:{once:!0},className:"flex flex-col sm:flex-row gap-4",children:[e.jsx("a",{href:"/MyResume",children:e.jsx(j,{title:"Resume"})}),e.jsx("a",{href:"https://t.me/+85586623507",children:e.jsx(j,{title:"Hire Me"})})]})]}),e.jsx(n.div,{variants:a,custom:1.2,initial:"hidden",whileInView:"visible",viewport:{once:!0},className:"flex justify-center items-center rounded-3xl drop-shadow-[0_4px_50px_rgba(239,68,68,0.7)] mt-12 md:mt-0 md:w-1/2",children:e.jsx(M,{})})]});export{C as default};
